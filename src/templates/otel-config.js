b01157c1af998d024967ba3c93ded15677e214b4
Thiago Freitas <thiagosistemas3@gmail.com>
2025-10-15 16:19:06 -0300

    Refactor createOtelEnvironmentVariables to include OTEL_PYTHON_KAFKA_PYTHON_INSTRUMENT and clean up whitespace

--- src/templates/otel-config.js
+++ src/templates/otel-config.js
@@ -2,7 +2,7 @@ const { generateSubsystemName } = require('../utils/deployment-utils');
 
 function createOtelEnvironmentVariables(namespace, deploymentName) {
   const subsystemName = generateSubsystemName(namespace, deploymentName);
-  
+
   return [
     {
       name: "OTEL_SERVICE_NAME",
@@ -37,6 +37,10 @@ function createOtelEnvironmentVariables(namespace, deploymentName) {
       value: "true"
     },
     {
+      name: "OTEL_PYTHON_KAFKA_PYTHON_INSTRUMENT",
+      value: "true"
+    },
+    {
       name: "OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED",
       value: "true"
     }
@@ -46,12 +50,13 @@ function createOtelEnvironmentVariables(namespace, deploymentName) {
 function getOtelVariableNames() {
   return [
     "OTEL_SERVICE_NAME",
-    "OTEL_RESOURCE_ATTRIBUTES", 
+    "OTEL_RESOURCE_ATTRIBUTES",
     "OTEL_IP",
     "OTEL_EXPORTER_OTLP_ENDPOINT",
     "OTEL_PYTHON_DJANGO_INSTRUMENT",
     "OTEL_PYTHON_REQUESTS_INSTRUMENT",
     "OTEL_PYTHON_PSYCOPG2_INSTRUMENT",
+    "OTEL_PYTHON_KAFKA_PYTHON_INSTRUMENT",
     "OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED"
   ];
 }
@@ -60,7 +65,7 @@ function validateOtelConfiguration(envVars, namespace, deploymentName) {
   const requiredVars = getOtelVariableNames();
   const existingVarNames = envVars.map(env => env.name);
   const missingVars = requiredVars.filter(varName => !existingVarNames.includes(varName));
-  
+
   const validation = {
     isComplete: missingVars.length === 0,
     missingVars,
@@ -68,18 +73,18 @@ function validateOtelConfiguration(envVars, namespace, deploymentName) {
     isCorrectServiceName: false,
     isCorrectResourceAttributes: false
   };
-  
+
   const serviceNameVar = envVars.find(env => env.name === 'OTEL_SERVICE_NAME');
   if (serviceNameVar) {
     validation.isCorrectServiceName = serviceNameVar.value === namespace;
   }
-  
+
   const resourceAttributesVar = envVars.find(env => env.name === 'OTEL_RESOURCE_ATTRIBUTES');
   if (resourceAttributesVar) {
     const expectedValue = `cx.application.name=${namespace},cx.subsystem.name=${generateSubsystemName(namespace, deploymentName)}`;
     validation.isCorrectResourceAttributes = resourceAttributesVar.value === expectedValue;
   }
-  
+
   return validation;
 }
 
